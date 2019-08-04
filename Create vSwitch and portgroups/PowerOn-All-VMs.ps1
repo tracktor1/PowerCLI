51dc363fd6c3ac3caf69f9860e0dcf8727628dd0
David <39525540+tracktor1@users.noreply.github.com>
2019-08-04 22:14:18 +0300

    Create PowerOn-All-VMs.ps1

---
+++ Create vSwitch and portgroups/PowerOn-All-VMs.ps1
@@ -0,0 +1,33 @@
+<#
+This script will power on all poweredoff VMs on all ESXIs connected to Vcenter.
+Change the vcenter address and admin user in the variables section.
+
+DA-#>
+
+#Variables
+$viadmin = 'root'
+$viserver = '192.168.100.41'
+
+# Connect to vcenter 
+#-------------------
+$credentials = Get-Credential -UserName $viadmin -Message "Enter your vCenter password"
+Connect-VIServer $viserver -Credential $credentials
+
+$VMHosts = Get-VMHost
+$AllVMs = get-vm
+
+Foreach ($VM in $AllVMs){
+	$Pstate = $VM.PowerState
+	$VMName = $VM.Name
+	#write-host $VMName power state is $Pstate
+	IF ($Pstate -eq 'PoweredOff'){
+		write-host Powering ON $VMName -ForegroundColor Green
+		Start-VM -VM $VMName -Confirm:$false
+		Start-sleep -s 5
+		}
+	ELSE { 
+		IF ($Pstate -eq 'PoweredOn'){
+		write-host $VMName is already powered ON -ForegroundColor Yellow
+		}
+	}
+}
